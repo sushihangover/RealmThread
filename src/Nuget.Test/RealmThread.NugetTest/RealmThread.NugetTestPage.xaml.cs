eb261eee81bcb266c8fae6d2383376cd4d605957
SushiHangover <sushihangover@outlook.com>
2016-12-06 00:14:34 -0800

    Add skeleton Nuget.Text Xamarin.Forms based apps

---
+++ src/Nuget.Test/RealmThread.NugetTest/RealmThread.NugetTestPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RealmThread.NugetTest
+{
+	public partial class RealmThread.NugetTestPage : ContentPage
+	{
+		public RealmThread.NugetTestPage ()
+		{
+			InitializeComponent();
+}
+	}
+}
